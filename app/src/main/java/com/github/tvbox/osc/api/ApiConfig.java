bc29767845fa4b06fd73a7a1aea9546cbb8e9a06
kkgit2008 <hicesamon@qq.com>
2024-06-14 15:46:04 +0800

    Update ApiConfig.java
    add url 666

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://textdb.online/yldMhb5lLac3pDyGURgtcElder666");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
