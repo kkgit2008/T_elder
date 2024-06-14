06d1ecf2d4373d943881136f45dfd8ee77444677
kkgit2008 <hicesamon@qq.com>
2024-06-14 14:34:39 +0800

    Update App.java
    添加默认配置：ijk使用硬解码，搜索结果显示成缩略图

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -67,6 +67,15 @@ public class App extends MultiDexApplication {
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
+        
+        //自定义默认配置: 硬解，缩略图
+
+        if (!Hawk.contains(HawkConfig.IJK_CODEC)) {            Hawk.put(HawkConfig.IJK_CODEC, "硬解码");        } 
+
+        if (!Hawk.contains(HawkConfig.SEARCH_VIEW)) {            Hawk.put(HawkConfig.SEARCH_VIEW, 1);        }
+        
+        //自定义默认配置.end 
+        
     }
 
     public static App getInstance() {
